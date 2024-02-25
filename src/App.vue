da6b19797fbe56c6a751a436ad9a5ad098e51c1b
Maruthi <singhkarthik261@gmail.com>
2024-02-25 13:18:16 +0530

    Password testing

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <input type="password" id="password" placeholder="Enter password" />
-    <button onclick="checkPassword()">Submit</button>
-    <div id="content" style="display: none">
-      <el-menu
-        :default-active="$router.currentRoute.path"
-        mode="horizontal"
-        :router="true"
-      >
-        <el-menu-item index="/">Interview Copilot</el-menu-item>
-        <el-menu-item index="/setting">Setting</el-menu-item>
-      </el-menu>
-      <router-view class="router_view" />
-    </div>
+    <el-menu
+      :default-active="$router.currentRoute.path"
+      mode="horizontal"
+      :router="true"
+    >
+      <el-menu-item index="/">Interview Copilot</el-menu-item>
+      <el-menu-item index="/setting">Setting</el-menu-item>
+    </el-menu>
+    <router-view class="router_view" />
   </div>
 </template>
 
@@ -40,28 +36,6 @@ export default {
   },
   methods: {},
 };
-function checkPassword() {
-  var password = document.getElementById("password").value;
-  if (sha256(password) === "1a9512b68107e4e2f2eb0acb60b50e7bb0a41dd0") {
-    document.getElementById("login-form").style.display = "none";
-    document.getElementById("content").style.display = "block";
-  } else {
-    alert("Incorrect password");
-  }
-}
-
-function sha256(plain) {
-  const crypto = window.crypto || window.msCrypto;
-  const encoder = new TextEncoder();
-  const data = encoder.encode(plain);
-  return crypto.subtle.digest("SHA-256", data).then((hash) => {
-    return Array.from(new Uint8Array(hash))
-      .map((byte) => {
-        return ("0" + (byte & 0xff).toString(16)).slice(-2);
-      })
-      .join("");
-  });
-}
 </script>
 <style>
 .router_view {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,71 +1,109 @@
 <template>
-  <div class="homeview_container">
-    <div class="center_container">
-      <div class="box">
-        <div class="func_desc">
-          <i class="el-icon-microphone"></i>
-          Speech Recognition Results
+  <input type="password" id="password" placeholder="Enter password" />
+  <button onclick="checkPassword()">Submit</button>
+  <div id="content" style="display: none">
+    <div class="homeview_container">
+      <div class="center_container">
+        <div class="box">
+          <div class="func_desc">
+            <i class="el-icon-microphone"></i>
+            Speech Recognition Results
+          </div>
+          <div v-if="!currentText" style="color: gray">No Content</div>
+          <div class="asr_content">{{ currentText }}</div>
+          <div class="single_part_bottom_bar">
+            <el-button
+              icon="el-icon-delete"
+              :disabled="!currentText"
+              @click="clearASRContent"
+            >
+              Clear Text
+            </el-button>
+          </div>
         </div>
-        <div v-if="!currentText" style="color: gray">No Content</div>
-        <div class="asr_content">{{ currentText }}</div>
-        <div class="single_part_bottom_bar">
-          <el-button icon="el-icon-delete" :disabled="!currentText" @click="clearASRContent">
-            Clear Text
-          </el-button>
+        <div class="box" style="border-left: none">
+          <div class="func_desc">
+            <i class="el-icon-s-custom"></i>
+            GPT Answer
+          </div>
+          <LoadingIcon v-show="show_ai_thinking_effect" />
+          <div class="ai_result_content">{{ ai_result }}</div>
+          <div class="single_part_bottom_bar">
+            <el-button
+              icon="el-icon-thumb"
+              @click="askCurrentText"
+              :disabled="!isGetGPTAnswerAvailable"
+            >
+              Ask GPT
+            </el-button>
+          </div>
         </div>
       </div>
-      <div class="box" style="border-left: none;">
-        <div class="func_desc">
-          <i class="el-icon-s-custom"></i>
-          GPT Answer
-        </div>
-        <LoadingIcon v-show="show_ai_thinking_effect"/>
-        <div class="ai_result_content">{{ ai_result }}</div>
-        <div class="single_part_bottom_bar">
-          <el-button icon="el-icon-thumb" @click="askCurrentText" :disabled="!isGetGPTAnswerAvailable">
-            Ask GPT
-          </el-button>
-        </div>
-      </div>
-    </div>
-    <div class="title_function_bar">
-      <el-button
+      <div class="title_function_bar">
+        <el-button
           type="success"
-          @click="startCopilot" v-show="state==='end'" :loading="copilot_starting"
-          :disabled="copilot_starting">Start Copilot
-      </el-button>
-      <el-button
+          @click="startCopilot"
+          v-show="state === 'end'"
+          :loading="copilot_starting"
+          :disabled="copilot_starting"
+          >Start Copilot
+        </el-button>
+        <el-button
           :loading="copilot_stopping"
-          @click="userStopCopilot" v-show="state==='ing'">Stop Copilot
-      </el-button>
-      <MyTimer ref="MyTimer"/>
+          @click="userStopCopilot"
+          v-show="state === 'ing'"
+          >Stop Copilot
+        </el-button>
+        <MyTimer ref="MyTimer" />
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import Assert from "assert-js"
+import Assert from "assert-js";
 import LoadingIcon from "@/components/LoadingIcon.vue";
 import MyTimer from "@/components/MyTimer.vue";
 import * as SpeechSDK from "microsoft-cognitiveservices-speech-sdk";
 import OpenAI from "openai";
-import config_util from "../utils/config_util"
+import config_util from "../utils/config_util";
+
+function checkPassword() {
+  var password = document.getElementById("password").value;
+  if (sha256(password) === "1a9512b68107e4e2f2eb0acb60b50e7bb0a41dd0") {
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("content").style.display = "block";
+  } else {
+    alert("Incorrect password");
+  }
+}
+
+function sha256(plain) {
+  const crypto = window.crypto || window.msCrypto;
+  const encoder = new TextEncoder();
+  const data = encoder.encode(plain);
+  return crypto.subtle.digest("SHA-256", data).then((hash) => {
+    return Array.from(new Uint8Array(hash))
+      .map((byte) => {
+        return ("0" + (byte & 0xff).toString(16)).slice(-2);
+      })
+      .join("");
+  });
+}
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   props: {},
   computed: {
     isDevMode() {
-      return (process.env.NODE_ENV === 'development')
+      return process.env.NODE_ENV === "development";
     },
     isGetGPTAnswerAvailable() {
       // return this.state === "ing" && !!this.currentText
-      return !!this.currentText
-
-    }
+      return !!this.currentText;
+    },
   },
-  components: {LoadingIcon, MyTimer},
+  components: { LoadingIcon, MyTimer },
   data() {
     return {
       currentText: "",
@@ -75,119 +113,132 @@ export default {
       copilot_stopping: false,
       show_ai_thinking_effect: false,
       popStyle: {},
-    }
+    };
   },
   async mounted() {
-    console.log("mounted")
+    console.log("mounted");
     if (this.isDevMode) {
       // this.currentText = demo_text
     }
   },
-  beforeDestroy() {
-  },
+  beforeDestroy() {},
   methods: {
     async askCurrentText() {
-      const apiKey = localStorage.getItem("openai_key")
-      let content = this.currentText
-      this.ai_result = ""
-      this.show_ai_thinking_effect = true
-      const model = config_util.gpt_model()
-      const gpt_system_prompt = config_util.gpt_system_prompt()
-      content = gpt_system_prompt + "\n" + content
+      const apiKey = localStorage.getItem("openai_key");
+      let content = this.currentText;
+      this.ai_result = "";
+      this.show_ai_thinking_effect = true;
+      const model = config_util.gpt_model();
+      const gpt_system_prompt = config_util.gpt_system_prompt();
+      content = gpt_system_prompt + "\n" + content;
 
       try {
         if (!apiKey) {
-          throw new Error("You should setup an Open AI Key!")
+          throw new Error("You should setup an Open AI Key!");
         }
 
-        const openai = new OpenAI({apiKey: apiKey, dangerouslyAllowBrowser: true})
+        const openai = new OpenAI({
+          apiKey: apiKey,
+          dangerouslyAllowBrowser: true,
+        });
         const stream = await openai.chat.completions.create({
           model: model,
-          messages: [{role: "user", content: content}],
+          messages: [{ role: "user", content: content }],
           stream: true,
         });
-        this.show_ai_thinking_effect = false
+        this.show_ai_thinking_effect = false;
 
         for await (const chunk of stream) {
-          const text = chunk.choices[0]?.delta?.content || ""
-          this.ai_result += text
+          const text = chunk.choices[0]?.delta?.content || "";
+          this.ai_result += text;
         }
       } catch (e) {
-        this.show_ai_thinking_effect = false
-        this.ai_result = "" + e
+        this.show_ai_thinking_effect = false;
+        this.ai_result = "" + e;
       }
     },
     clearASRContent() {
-      this.currentText = ""
+      this.currentText = "";
     },
     async startCopilot() {
-      this.copilot_starting = true
-      const token = localStorage.getItem("azure_token")
-      const region = config_util.azure_region()
-      const language = config_util.azure_language()
-      const openai_key = localStorage.getItem("openai_key")
-      console.log({region, language})
+      this.copilot_starting = true;
+      const token = localStorage.getItem("azure_token");
+      const region = config_util.azure_region();
+      const language = config_util.azure_language();
+      const openai_key = localStorage.getItem("openai_key");
+      console.log({ region, language });
       try {
         if (!openai_key) {
-          throw new Error("You should setup Open AI API Token")
+          throw new Error("You should setup Open AI API Token");
         }
         if (!token) {
-          throw new Error("You should setup Azure token")
+          throw new Error("You should setup Azure token");
         }
         if (!region) {
-          throw new Error("You should setup Azure region")
+          throw new Error("You should setup Azure region");
         }
 
-        const speechConfig = SpeechSDK.SpeechConfig.fromSubscription(token, region);
+        const speechConfig = SpeechSDK.SpeechConfig.fromSubscription(
+          token,
+          region
+        );
         speechConfig.speechRecognitionLanguage = language;
         const audioConfig = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
-        this.recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
+        this.recognizer = new SpeechSDK.SpeechRecognizer(
+          speechConfig,
+          audioConfig
+        );
       } catch (e) {
-        this.currentText = e
-        this.copilot_starting = false
-        return
+        this.currentText = e;
+        this.copilot_starting = false;
+        return;
       }
 
-      const recognizer = this.recognizer
-      const sdk = SpeechSDK
-
+      const recognizer = this.recognizer;
+      const sdk = SpeechSDK;
 
       recognizer.recognized = (sender, event) => {
-        if (sdk.ResultReason.RecognizedSpeech === event.result.reason && event.result.text.length > 0) {
-          const text = event.result.text
-          this.currentText = this.currentText + "\n" + text
+        if (
+          sdk.ResultReason.RecognizedSpeech === event.result.reason &&
+          event.result.text.length > 0
+        ) {
+          const text = event.result.text;
+          this.currentText = this.currentText + "\n" + text;
         } else if (sdk.ResultReason.NoMatch === event.result.reason) {
-          console.log("Speech could not be recognized")
+          console.log("Speech could not be recognized");
         }
       };
 
       recognizer.startContinuousRecognitionAsync(
-          () => {
-            this.copilot_starting = false
-            this.state = "ing"
-            this.$refs.MyTimer.start()
-            window.console.log("recognition started");
-          },
-          (err) => {
-            this.copilot_starting = false
-            this.currentText = "Start Failed:" + err
-            window.console.error("recogniton start failed", err);
-          })
+        () => {
+          this.copilot_starting = false;
+          this.state = "ing";
+          this.$refs.MyTimer.start();
+          window.console.log("recognition started");
+        },
+        (err) => {
+          this.copilot_starting = false;
+          this.currentText = "Start Failed:" + err;
+          window.console.error("recogniton start failed", err);
+        }
+      );
     },
     userStopCopilot() {
-      this.copilot_stopping = true
-      this.recognizer.stopContinuousRecognitionAsync(() => {
-        console.log("stoped")
-        this.copilot_stopping = false
-        this.state = "end"
-        this.$refs.MyTimer.stop()
-      }, (err) => {
-        console.log("err:", err)
-      })
-    }
-  }
-}
-
+      this.copilot_stopping = true;
+      this.recognizer.stopContinuousRecognitionAsync(
+        () => {
+          console.log("stoped");
+          this.copilot_stopping = false;
+          this.state = "end";
+          this.$refs.MyTimer.stop();
+        },
+        (err) => {
+          console.log("err:", err);
+        }
+      );
+    },
+  },
+};
 
 const demo_text = `
 Hello, thank you for coming for the interview. Please introduce yourself.
@@ -199,18 +250,15 @@ Alright, let me ask you a machine learning question.
 Sure, go ahead.
 
 Can you explain the Hidden Markov Model?
-`
+`;
 
 async function sleep(ms) {
-  return new Promise((resolve => setTimeout(resolve, ms)))
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 .homeview_container {
   display: flex;
   flex-direction: column;
@@ -242,7 +290,6 @@ async function sleep(ms) {
   flex-grow: 1;
 }
 
-
 .func_desc {
   text-align: center;
 }
@@ -254,7 +301,6 @@ async function sleep(ms) {
 .single_part_bottom_bar > .el-button {
   flex-grow: 1;
 }
-
 
 .ai_result_content {
   overflow-y: auto;
@@ -278,5 +324,4 @@ async function sleep(ms) {
   color: red;
   text-align: center;
 }
-
 </style>
