45791b3e38e7529b32ce7946c537193e018a76c0
Maruthi <singhkarthik261@gmail.com>
2024-02-25 13:10:29 +0530

    Password protection added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
-    <el-menu :default-active="$router.currentRoute.path" mode="horizontal" :router="true">
-      <el-menu-item index="/">Interview Copilot</el-menu-item>
-      <el-menu-item index="/setting">Setting</el-menu-item>
-    </el-menu>
-    <router-view class="router_view"/>
+    <input type="password" id="password" placeholder="Enter password" />
+    <button onclick="checkPassword()">Submit</button>
+    <div id="content" style="display: none">
+      <el-menu
+        :default-active="$router.currentRoute.path"
+        mode="horizontal"
+        :router="true"
+      >
+        <el-menu-item index="/">Interview Copilot</el-menu-item>
+        <el-menu-item index="/setting">Setting</el-menu-item>
+      </el-menu>
+      <router-view class="router_view" />
+    </div>
   </div>
 </template>
 
@@ -15,30 +23,45 @@
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
-
 </style>
 <script>
-import {mapGetters} from 'vuex';
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'App',
+  name: "App",
   props: {},
   computed: {},
-  beforeMount() {
-  },
-  mounted() {
-
-  },
+  beforeMount() {},
+  mounted() {},
   data() {
     return {
-      activeIndex: "/"
-    }
+      activeIndex: "/",
+    };
   },
-  methods: {}
-
-
+  methods: {},
+};
+function checkPassword() {
+  var password = document.getElementById("password").value;
+  if (sha256(password) === "1a9512b68107e4e2f2eb0acb60b50e7bb0a41dd0") {
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("content").style.display = "block";
+  } else {
+    alert("Incorrect password");
+  }
 }
 
+function sha256(plain) {
+  const crypto = window.crypto || window.msCrypto;
+  const encoder = new TextEncoder();
+  const data = encoder.encode(plain);
+  return crypto.subtle.digest("SHA-256", data).then((hash) => {
+    return Array.from(new Uint8Array(hash))
+      .map((byte) => {
+        return ("0" + (byte & 0xff).toString(16)).slice(-2);
+      })
+      .join("");
+  });
+}
 </script>
 <style>
 .router_view {
